b3f6800607fe6c197ebda1681894ba80b33790cb
Revxrsal <aloosh.kasasbeh@gmail.com>
2025-05-01 20:56:32 +0300

    improve examples

--- src/test/java/revxrsal/spec/test/ServerConfig.java
+++ src/test/java/revxrsal/spec/test/ServerConfig.java
@@ -47,11 +47,67 @@ public interface ServerConfig {
         return false;
     }
 
+    // You can leave it like this with no implementation
     @Save
     void save();
 
+    // Or write as a default function which will be called
+    // after the plugin is reloaded.
+    //
+    // Your function can have any arguments as needed
     @Reload
-    void reload();
+    default void reload() {
+        System.out.println("Plugin reloaded!");
+    }
+
+    // We can nest ConfigSpecs too!
+    //
+    // You can also have them inside Maps, Lists, Sets, arrays, etc.
+    @Comment("The server messages")
+    Messages messages();
+
+    @Comment("Includes numerical values")
+    ServerNumbers numbers();
+
+    @ConfigSpec
+    interface ServerNumbers {
+
+        @Comment("The chunk radius")
+        default int chunkRadius() {
+            return 13;
+        }
+
+        // @Memoize allows us to compute certain values and cache their result
+        //
+        // This is very useful for heavy, repetitive computations.
+        //
+        // Note: @Memoize does not (yet) consider arguments when caching values.
+        // Therefore, it is best to just use it to compute the parts that depend on the
+        // configuration values
+        //
+        // Reloading, resetting, or calling a setter will re-compute all memoized values.
+        //
+        // See the docs of @Memoize for more info
+        @Memoize
+        default int chunkRadiusCubed() {
+            System.out.println("Computing chunkRadius^3");
+            return chunkRadius() * chunkRadius() * chunkRadius();
+        }
+    }
+
+    @ConfigSpec
+    interface Messages {
+
+        @Comment("Sent when a player joins")
+        default String playerJoined() {
+            return "Player %player% joined the server!";
+        }
+
+        @Comment("Sent when a player leaves")
+        default String playerLeft() {
+            return "Player %player% left the server!";
+        }
+    }
 
     enum Mode {
         SURVIVAL,
